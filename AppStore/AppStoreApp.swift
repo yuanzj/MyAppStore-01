d72f3a30244a35652f13f9166f87485f374f3d81
袁志健 <jswxyzj@qq.com>
2022-03-06 22:27:43 +0800

    Initial Commit

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by 袁志健 on 2022/3/6.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
